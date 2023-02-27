aa343159a0057623d6afb395b46d8b3fa2b559b9
buffreak <revihammer@gmail.com>
2023-02-27 13:22:20 +0700

    fix undefined index

--- function.php
+++ function.php
@@ -8,6 +8,9 @@ function readDirRecursive($path = "data", $search = "", $fileOnly = true){
   $walks = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::UNIX_PATHS));
   foreach($walks as $key => $walk){
     if(@strpos($walk->getPathname(), $search) !== false || empty($search)){
+      if(stripos($walk->getPathname(), "privacy-policy.html") !== false || stripos($walk->getPathname(), "disclaimer.html") !== false || stripos($walk->getPathname(), "dmca.html") !== false) {
+        continue;
+      }
       if($fileOnly && $walk->isFile()){
         $container[] = [$walk->getPathname(), $walk->getMTime()];
       }else if (!$fileOnly && ($walk->isDir() || $walk->isFile())){
